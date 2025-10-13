32f36118738fc8af430cbc6f460187949a444896
Angel Gabriel Lopez <contact@imrlopez.dev>
2025-10-13 11:16:08 -0400

    refactor: improve handler structure and add isNextApiRequest check

--- src/startServerAndCreateHandler.ts
+++ src/startServerAndCreateHandler.ts
@@ -1,59 +1,97 @@
-import { getBody } from './lib/getBody';
-import { getHeaders } from './lib/getHeaders';
-import type { ApolloServer, BaseContext, ContextFunction } from '@apollo/server';
-import type { ServerResponse } from 'node:http';
-import { parse } from 'node:url';
+import type { ServerResponse } from 'node:http'
+import { parse } from 'node:url'
+import type { ApolloServer, BaseContext, ContextFunction } from '@apollo/server'
+import { getBody } from './lib/getBody'
+import { getHeaders } from './lib/getHeaders'
+import { isNextApiRequest } from './lib/isNextApiRequest'
 
 interface Options<Req extends Request, Context extends BaseContext> {
-  context?: ContextFunction<[Req], Context>;
+	context?: ContextFunction<[Req], Context>
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const defaultContext: ContextFunction<[], any> = async () => ({});
+const defaultContext: ContextFunction<[], any> = async () => ({})
 
-function startServerAndCreateHandler<Req extends Request = Request, Context extends BaseContext = object>(
-  server: ApolloServer<Context>,
-  options?: Options<Req, Context>,
-) {
-  server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
+function startServerAndCreateHandler<
+	Req extends Request = Request,
+	Context extends BaseContext = object,
+>(server: ApolloServer<Context>, options?: Options<Req, Context>) {
+	server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests()
 
-  const contextFunction = options?.context || defaultContext;
+	const contextFunction = options?.context || defaultContext
 
-  async function handler(req: Req, res: ServerResponse) {
-    const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
-      context: () => contextFunction(req),
-      httpGraphQLRequest: {
-        body: await getBody(req),
-        headers: getHeaders(req),
-        method: req.method || 'POST',
-        search: req.url ? parse(req.url).search || '' : '',
-      },
-    });
+	async function handler<HandlerReq extends Req>(
+		req: HandlerReq,
+		res: ServerResponse,
+	): Promise<unknown>
+	async function handler<HandlerReq extends Req>(
+		req: HandlerReq,
+		res?: undefined,
+	): Promise<Response>
+	async function handler(req: Req, res: ServerResponse | undefined) {
+		const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
+			context: () => contextFunction(req),
+			httpGraphQLRequest: {
+				body: await getBody(req),
+				headers: getHeaders(req),
+				method: req.method || 'POST',
+				search: req.url ? parse(req.url).search || '' : '',
+			},
+		})
 
+		if (isNextApiRequest(req)) {
+      if (!res) {
+        throw new Error('Response object is required for Next.js API routes')
+			}
+			for (const [key, value] of httpGraphQLResponse.headers) {
+        res.setHeader(key, value)
+			}
+      
+			// Set status code
+			res.statusCode = httpGraphQLResponse.status || 200
+      
+			// Handle complete response body
+			if (httpGraphQLResponse.body.kind === 'complete') {
+        res.end(httpGraphQLResponse.body.string)
+				return
+			}
+      
+			// Handle chunked response body
+			if (httpGraphQLResponse.body.kind === 'chunked') {
+        for await (const chunk of httpGraphQLResponse.body.asyncIterator) {
+          res.write(chunk)
+				}
+				res.end()
+			}
+		}
+    
     // Set response headers
+    const headers: Record<string, string> = {};
     for (const [key, value] of httpGraphQLResponse.headers) {
-      res.setHeader(key, value);
+      headers[key] = value;
     }
 
-    // Set status code
-    res.statusCode = httpGraphQLResponse.status || 200;
+    return new Response(
+      httpGraphQLResponse.body.kind === 'complete'
+        ? httpGraphQLResponse.body.string
+        : new ReadableStream({
+            async pull(controller) {
+              if (httpGraphQLResponse.body.kind === 'chunked') {
+                const { value, done } = await httpGraphQLResponse.body.asyncIterator.next();
 
-    // Handle complete response body
-    if (httpGraphQLResponse.body.kind === 'complete') {
-      res.end(httpGraphQLResponse.body.string);
-      return;
-    }
+                if (done) {
+                  controller.close();
+                } else {
+                  controller.enqueue(value);
+                }
+              }
+            },
+          }),
+      { headers, status: httpGraphQLResponse.status || 200 },
+    );
+	}
 
-    // Handle chunked response body
-    if (httpGraphQLResponse.body.kind === 'chunked') {
-      for await (const chunk of httpGraphQLResponse.body.asyncIterator) {
-        res.write(chunk);
-      }
-      res.end();
-    }
-  }
-
-  return handler;
+	return handler
 }
 
-export { startServerAndCreateHandler };
+export { startServerAndCreateHandler }
